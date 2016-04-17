477e63aaf86d78cff8a78f6b3e34929959747ed6
Shreesha Aithal <9211shreesha@gmail.com>
2016-04-17 22:19:13 +0530

    Moar features!!
    Get module names and add them to a hash map

--- parser/kmp.cpp
+++ parser/kmp.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 int kmpSearch(string, string);
 void computeLongestPrefix(string, int, int*);
+int kmpSearch(string, string, vector<string>&);
+
+extern int KMP_FLAG;
 
 int kmpSearch(string text, string pattern)
 {
@@ -32,6 +35,7 @@ int kmpSearch(string text, string pattern)
                 }
 
                 if (index_pattern == length_of_pattern) {
+                        //cout <<"Pattern found at index " <<index_text - index_pattern <<endl;
                         counter_for_line++;
 
                         if ((KMP_FLAG == 0) || (KMP_FLAG == 1))
@@ -49,6 +53,61 @@ int kmpSearch(string text, string pattern)
         free(longest_proper_prefix);
 
         return counter_for_line;
+}
+
+int kmpSearch(string text, string pattern, vector<string> &modules)
+{
+        int length_of_text = text.length();
+        int length_of_pattern = pattern.length();
+
+        int *longest_proper_prefix = (int*) malloc(sizeof(int) * length_of_pattern);
+
+        int index_pattern;
+        int index_text;
+        int index_module_name;
+        int match;
+        string module_name;
+
+        computeLongestPrefix(pattern, length_of_pattern, longest_proper_prefix);
+
+        index_text = 0;
+        index_pattern = 0;
+        index_module_name = 0;
+        match = 0;
+
+        while (index_text < length_of_text) {
+                if (pattern[index_pattern] == text[index_text]) {
+                        index_pattern++;
+                        index_text++;
+                }
+                if (index_pattern == length_of_pattern) {
+                        index_module_name = index_text - index_pattern;
+
+                        for (int index = index_module_name; index < text.size(); index++) {
+                                if (text[index] == '>')
+                                        break;
+
+                                module_name.push_back(text[index]);
+                        }
+
+                        modules.push_back(module_name);
+
+                        match = 1;
+
+                        break;
+
+                        index_pattern = longest_proper_prefix[index_pattern - 1];
+                } else if ((index_text < length_of_text) && (pattern[index_pattern] != text[index_text])) {
+                        if (index_pattern != 0)
+                                index_pattern = longest_proper_prefix[index_pattern - 1];
+                        else
+                                index_text = index_text + 1;
+                }
+        }
+
+        free(longest_proper_prefix);
+
+        return match;
 }
 
 void computeLongestPrefix(string pattern, int length_of_pattern, int *longest_proper_prefix)
@@ -76,3 +135,4 @@ void computeLongestPrefix(string pattern, int length_of_pattern, int *longest_pr
                 }
         }
 }
+
--- parser/parser.cpp
+++ parser/parser.cpp
@@ -2,20 +2,35 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
 #include <cstring>
+
+#define KMP_LEVEL 0
+#define KMP_MODULES 1
+#define KMP_MODULENAMES 2
 
 using namespace std;
 
+int KMP_FLAG;
+
 extern int kmpSearch(string, string);
+extern int kmpSearch(string, string, vector<string>&);
 
 void readFileToString(char*, vector<string>&);
-int getLevelCount(vector<string> &lines_from_file);
+int getLevelCount(vector<string>&);
 int getModuleCount(vector<string>&);
+void getModuleNames(vector<string>&, vector<string>&);
 
 int main(int argc, char *argv[])
 {
         int level_count;
+        int module_count;
+        int iterator;
+
         vector<string> lines_from_file;
+        vector<string> module_names;
+
+        map<int, string> map_module_names;
 
         readFileToString(argv[1], lines_from_file);
 
@@ -25,6 +40,15 @@ int main(int argc, char *argv[])
         cout <<"Levels = " <<level_count <<endl;
         cout <<"Total number of modules = " <<module_count <<endl;
 
+        getModuleNames(lines_from_file, module_names);
+
+        for (iterator = 0; iterator < module_names.size(); iterator++) {
+        map_module_names[iterator] = module_names[iterator];
+        }
+
+        for (iterator = 0; iterator < map_module_names.size(); iterator++) {
+               cout <<"Module: " << map_module_names[iterator] <<endl;
+        }
 
         return 0;
 }
@@ -36,7 +60,7 @@ int getModuleCount(vector<string> &lines_from_file)
         int iterator;
 
         //setting the KMP_FLAG to get the number of modules.
-        KMP_FLAG = 1;
+        KMP_FLAG = KMP_MODULES;
 
         counter = 0;
         pattern = "module";
@@ -49,13 +73,29 @@ int getModuleCount(vector<string> &lines_from_file)
 }
 
 
+void getModuleNames(vector<string> &lines_from_file, vector<string> &module_names)
+{
+        string pattern;
+        int iterator;
+
+        //setting the KMP_FLAG to get the number of modules.
+        KMP_FLAG = KMP_MODULENAMES;
+
+        pattern = "module";
+        for (iterator = 0; iterator < lines_from_file.size(); iterator++) {
+                if (kmpSearch(lines_from_file[iterator], pattern, module_names)) {
+                        ;
+                }
+        }
+}
+
 int getLevelCount(vector<string> &lines_from_file)
 {
         int counter;
         int iterator;
 
         // setting the KMP_FLAG to get the number of levels.
-        KMP_FLAG = 0;
+        KMP_FLAG = KMP_LEVEL;
 
         string pattern = "level";
         counter = 0;
