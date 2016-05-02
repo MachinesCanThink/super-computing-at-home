97f5c0ec59b7c54138fdbcdde8b8141b7709e824
Shreesha Aithal <9211shreesha@gmail.com>
2016-05-02 07:55:58 +0530

    finished writing the interfaces of the task_vector

--- task_manager/node.cpp
+++ task_manager/node.cpp
@@ -18,8 +18,11 @@ typedef struct node
 	//string module_name;
 } node;
 
+extern vector<int> getDependentModuleIds(int);
+
 void declareVector(int, vector<int>);
-int fillVector(vector< vector<string> >, vector<int>, vector<string>, map<string, int>, int[]);
+void printVector(void);
+int fillVector(vector< vector<string> >, vector<int>, vector<string>, map<string, int>, map<string, int>);
 
 vector<int> num_modules_in_levels;
 int num_levels;
@@ -39,32 +42,47 @@ void declareVector(int level_count, vector<int> modules_in_level)
 	task_vector.resize(level_count);
 
 	for (iterator = 0; iterator < level_count; iterator++) {
-		task_vector.resize(modules_in_level[counter]);
-		counter++;
+		task_vector[iterator].resize(modules_in_level[iterator]);
 	}
 }
 
-int Vector(vector< vector<string> > depend_modules, vector<int> level_details, vector<string> module_names, map<string, int> module_name_and_id, int num_deps[])
+int fillVector(vector< vector<string> > depend_modules, vector<int> level_details, vector<string> module_names, map<string, int> module_name_and_id, map<string, int> num_deps)
 {
 	int iterator;
 	int iterator2;
-	int counter;
 	int counter1;
 
-	counter = 0;
 	counter1 = 0;
 
-	for (iterator = 0; iterator < num_levels; iterator++) {
-		for (iterator2 = 0; iterator2 < num_modules_in_levels[counter]; iterator2++, counter1++) {
+	for (iterator = 0; iterator < task_vector.size(); iterator++) {
+		for (iterator2 = 0; iterator2 < task_vector[iterator].size(); iterator2++, counter1++) {
 			task_vector[iterator][iterator2].module_id = module_name_and_id[module_names[counter1]];
-
 			task_vector[iterator][iterator2].level = level_details[counter1];
-			
-			task_vector[iterator][iterator2].num_dependencies = num_deps[counter1];
+			task_vector[iterator][iterator2].num_dependencies = num_deps[module_names[counter1]];
+			task_vector[iterator][iterator2].dep_id = getDependentModuleIds(counter1);
 		}
-
-		counter++;
 	}
 
 	return 1;
+}
+
+void printVector(void) 
+{
+	int iterator;
+	int iterator2;
+
+	for (iterator = 0; iterator < task_vector.size(); iterator++) {
+		for (iterator2 = 0; iterator2 < task_vector[iterator].size(); iterator2++) {
+			cout <<"Module ID: " <<task_vector[iterator][iterator2].module_id <<endl;
+			cout <<"Module level: " <<task_vector[iterator][iterator2].level <<endl;
+			cout <<"Number of dependencies : " <<task_vector[iterator][iterator2].num_dependencies <<endl;
+
+			cout <<"ID's of dependent modules : ";
+			for (int i = 0; i < task_vector[iterator][iterator2].dep_id.size(); i++) {
+				cout <<task_vector[iterator][iterator2].dep_id[i] <<"  ";
+			}
+
+			cout <<endl <<endl;
+		}
+	}
 }
