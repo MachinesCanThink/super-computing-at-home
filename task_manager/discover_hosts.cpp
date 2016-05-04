4a67486dd8fdafa581304844bcd8516e94f5b202
Shreesha Aithal <9211shreesha@gmail.com>
2016-05-04 08:57:48 +0530

    fix host discovery

---
+++ task_manager/discover_hosts.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+using namespace std;
+
+int discover(string);
+
+int main(int argc, char *argv[]) 
+{
+	discover("192.168.0.1/24");
+
+	return 0;
+}
+
+int discover(string ip_address)
+{
+	string command_interm;
+	int status;
+	char *command;
+	int count  = 0;
+
+	int i, j;
+
+	string line;
+	ifstream input;
+	ofstream output;
+
+	command_interm = "sudo nmap -sP --send-ip -oG discovered_hosts.tmp ";
+
+	command_interm.append(ip_address);
+	cout <<"command: " <<command_interm <<endl;
+
+	command =new char[command_interm.length() + 1];
+	strcpy(command, command_interm.c_str());
+
+	system(command);
+
+	input.open("discovered_hosts.tmp", ifstream::in);
+	output.open("discovered_hosts", ifstream::out);
+
+	if (input.is_open()) {
+        while (getline(input, line)) {
+           	if (line[0] == '#') {
+ 				;
+           	} else {
+           		string sub_text;
+           		for (i = 6, j = 0; i < line.size(); i++, j++) {
+           			if (line[i+1] != '(') 
+           				sub_text.push_back(line[i]);
+           			else
+           				break; 	
+           		}
+
+           		if (output.is_open())
+           			output <<sub_text <<endl;
+           	}
+        }
+    }
+
+    input.close();
+    output.close();
+
+	return 1;
+}
